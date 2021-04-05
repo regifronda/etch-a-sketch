3530df87f56a6925e8db70bc54fc1c4af160766e
Regi Fronda <regisfronda@gmail.com>
2021-04-05 06:05:09 -0700

    made 16 x 16 grid

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const container = document.getElementById("container");
+function makeGrid(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let square = document.createElement("div");
+        container.appendChild(square).className = "grid-item";
+    };
+};
 
+makeGrid(16, 16);
