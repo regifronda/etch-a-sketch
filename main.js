baf984c3f3adac6f45ffb2c6a99a6dd1118e9c12
Regi Fronda <regisfronda@gmail.com>
2021-04-08 02:01:41 -0700

    added button that clears current grid

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const newGridButton = document.getElementById("newGridButton");
+
 const container = document.getElementById("container");
 function makeGrid(rows, cols) {
     container.style.setProperty("--grid-rows", rows);
@@ -11,4 +13,17 @@ function makeGrid(rows, cols) {
         container.appendChild(square).className = "grid-item";
     };
 };
+
+function clearGrid () {
+    let square = document.getElementsByClassName("grid-item");
+    for (i = 0; i < square.length; i++) {
+        square[i].style.backgroundColor = "white";
+        square[i].style.color = "black";
+    };
+};
+
 makeGrid(16, 16);
+
+newGridButton.addEventListener("click", function() {
+    clearGrid();
+});
