ba6fe7e315a8d54f27f8c8a41f969e25518cd407
Regi Fronda <regisfronda@gmail.com>
2021-04-08 01:02:26 -0700

    have squares change to black when mouse cursor hovers by changing div's background color

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ function makeGrid(rows, cols) {
     container.style.setProperty("--grid-cols", cols);
     for (i = 0; i < (rows * cols); i++) {
         let square = document.createElement("div");
+        square.onmouseover = function() {
+            square.style.backgroundColor = "black";
+            square.style.color = "white";
+        };
         container.appendChild(square).className = "grid-item";
     };
 };
-
 makeGrid(16, 16);
